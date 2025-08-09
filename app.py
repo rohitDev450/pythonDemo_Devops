53dbd2d62f69b4d83bc5b4e22980bce64c3b4993
Rohit Aute <157054733+rohitDev450@users.noreply.github.com>
2025-08-09 05:31:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return "Hello, DevOps Ci CD! pipleline is working fine ok."
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
